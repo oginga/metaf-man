888e72a1ae179ac2f798b4eee943a80a0eae89f3
oginga <stevilley@gmail.com>
2016-01-15 15:38:07 +0300

    protoypr:db manipulation

--- mfm/main.py
+++ mfm/main.py
@@ -6,7 +6,7 @@ import json
 import MySQLdb
 
 current_dir=os.getcwd()
-meta_file_name='.meta'
+meta_file_name='init.json'
 
 '''
 
@@ -18,32 +18,30 @@ def Creae_DB_If_None_exist():
 '''
 
 
-def DB_connection(sql):
+def store_into_DB(**kwargs):
 
-	con=MySQLdb.connect('localhost','root','526419','Metar')
-	cursor=con.cursor()
-
+	db=MySQLdb.connect('localhost','root','526419','mfm')
+	cursor=db.cursor()
+	sql="INSERT INTO meta(path,metadata) VALUES ('%s','%s')"%(str(kwargs['path']),str(kwargs['metadata']))
+	
+	
 	try:
 		cursor.execute(sql)
+		db.commit()
+		print 'database updated'
+
 	except:
 		db.rollback
-
+		print "UNABLE"
 	db.close()
 
-
-
-def createMetaFile():
+def createMetaFile():   
 	with open(os.path.join(os.getcwd(),meta_file_name),'w') as metafile:
 		#file_path=
-		init_data='''
-					{
-					'path':%s,
-					'metadata':[],
-					}
-
-				  '''%(os.path.abspath(os.path.join(os.getcwd(),meta_file_name)),)
+		init_data={'path':str(os.path.abspath(os.path.join(os.getcwd(),meta_file_name)),),'metadata':[]}
+		
 		json.dump(init_data,metafile,indent=0)
-
+		return True
 def initDir(dir_name):
 	if not os.path.isdir(dir_name):
 		print "Creating directory..."
@@ -57,28 +55,27 @@ def initDir(dir_name):
 		if createMetaFile():
 				print "successfully created .meta file"
 				with open(meta_file_name) as _meta:
-					meta_data=json.load(_meta)
-					#create an sql statement using the key-values in meta_data and execute
+					meta_data=json.loads(_meta.read())
+					print type(meta_data)
+					print meta_data 
+				#try:
+					store_into_DB(path=meta_data['path'],metadata=meta_data['metadata'])
+				#except:
+				#	print "Error occured in the storage of path and meta-data"
 
 
-
-
-		else:pass
+		else:
+			print "Metafile creation unsuccessfull"
 			#If init successfull,register path to that directory in the DBs lookup paths
 			#Store a copy of the current dirs meta_info int the DB
-			#
-			#
-			#
-			#
-		
+			#Update the database and manipulate the json with the new oibje
+			
 	else:
 		print 'DIR exists.Changing to that directory'
 		os.chdir(dir_name)
 		print os.getcwd()
 		if os.path.isfile(meta_file_name):
 			print True
-
-
 
 
 	
